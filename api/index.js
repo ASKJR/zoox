0b7688ebe328a8f071b9c203c744410c74b6d59d
Alberto Kato <albertokatojr@gmail.com>
2021-02-13 23:48:53 +0000

    fixed variables in docker-compose & index

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ app.all('*', (req, res, next) => {
 app.use(globalErrHandler);
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_DB_URI, {
